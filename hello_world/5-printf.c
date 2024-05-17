730269c91bf949e45e2e1a175c5226430e9b22c6
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-17 04:42:04 +0000

    added 5 and program

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main -prints exactly with proper grammar, but the outcome is a piece of art.
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
