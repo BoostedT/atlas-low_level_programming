c3834fad2d5eab42d9a6e04f482b1a8196a8d8a7
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-18 23:11:45 +0000

    added 4 and a program the prints all but q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+while (c <= 'z')
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+c++;
+}
+putchat('\n');
+return (0);
+}
