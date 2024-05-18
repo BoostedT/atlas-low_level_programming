c4f0e706bbcbde18599400cf0ee00c7dcee664be
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-18 23:37:25 +0000

    added 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet backwards
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c >= 'a')
+{
+putchar(a);
+c--;
+}
+putchar('\n');
+return (0);
+}
