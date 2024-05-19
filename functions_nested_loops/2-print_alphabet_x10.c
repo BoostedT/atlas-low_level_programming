71cdf9bc3c4aa1bf0156f830b9230764fac68161
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-19 20:25:25 +0000

    added 2 and program

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabetx10 - prints alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabetx10(void)
+{
+int i = 0;
+while (i < 10)
+{
+t = 'a';
+while ( t <= 'z')
+{
+_putchar (t);
+t++;
+}
+_putchar('\n');
+i++;
+}
+}
