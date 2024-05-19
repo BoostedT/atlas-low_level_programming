d6c924a5e8c129bc2333c44cec6736d6c9ecb73a
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-19 18:50:42 +0000

    added 9 and program that prints all single digit combos

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - program that prints all possible combinations of single-digit numbers.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+putchar(',');
+return (0);
+}
