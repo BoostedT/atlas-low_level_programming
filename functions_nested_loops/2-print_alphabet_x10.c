75b1374566271a609eabbddab112f5a01201c0d5
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-19 20:30:40 +0000

    recodeD

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabetx10 - prints alphabet 10 times
+ * print_alphabet_x10 - prints alphabet 10 times
  *
  * Return: 0
  */
-void print_alphabetx10(void)
+void print_alphabet_x10(void)
 {
 int i = 0;
 char t = 'a';
