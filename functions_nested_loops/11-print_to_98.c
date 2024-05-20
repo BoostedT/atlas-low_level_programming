861f98827fc93d5c7fb6efe3dbd5e165db1cb36b
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-20 20:46:55 +0000

    fixed code

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,28 +6,26 @@
  */
 void print_to_98(int n)
 {
-int t, w;
+int i, j;
 
 if (n <= 98)
 {
-for (t = n; t <= 98; t++)
+for (i = n; i <= 98; i++)
 {
-if (t == 98)
-{
-printf("%d\n", t);
-else if (t != 98)
-printf("%d, ", t);
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
 }
 }
 else if (n >= 98)
 {
-for (w = n; w >= 98; w--)
+for (j = n; j >= 98; j--)
 {
-if (w == 98)
-printf("%d\n", w);
-else if (w != 98)
-printf("%d, ", w);
-}
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
 }
 }
 }
