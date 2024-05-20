53bbd145b1a22744b4437493cf7db44128549a00
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-20 20:01:31 +0000

    added 6 and code

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+/**
+ * _abs - prints the sign of absolute value of a number
+ * @n: The number to be checked
+ * Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
