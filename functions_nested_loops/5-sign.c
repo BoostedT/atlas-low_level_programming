ef815af1b7e3f326344bf41fc05d49b2bfe83f3c
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-20 19:46:56 +0000

    added 5 and programs

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: prints the sign of a number
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ */
+int print_sign(int n);
+{
+while (n > 0)
+{
+_putchar('+');
+return (1);
+while (n == 0)
+{
+_putchar('0');
+return (0);
+while (n < 0)
+{
+putchar('-');
+return (-1);
+}
+}
+}
+}
