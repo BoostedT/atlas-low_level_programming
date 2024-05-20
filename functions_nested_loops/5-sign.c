ee30658984571a03bd42cfaba8c216e317ffe800
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-20 19:52:47 +0000

    corrected code

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,15 +10,17 @@ while (n > 0)
 {
 _putchar('+');
 return (1);
-while (n == 0)
+}
+else if (n == 0)
 {
 _putchar('0');
 return (0);
-while (n < 0)
+}
+else (n < 0)
 {
 putchar('-');
 return (-1);
 }
 }
-}
-}
+
+
