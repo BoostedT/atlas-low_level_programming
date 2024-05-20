57d0f2a910e23237b9f8950b64ff04070203445c
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-20 20:27:37 +0000

    added times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,24 +7,28 @@ void times_table(void)
 {
 int i, j, result;
 
-for (i = 0; i < 9; i++)
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
 {
 result = i * j;
-if (j != 10)
+if (j == 0)
+{
+_putchar('0' + result);
+}
+if (result < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
-}
-else
-{
 _putchar(' ');
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
+_putchar('0' + result);
 }
-if (j < 9)
+else if (result >= 10)
 {
 _putchar(',');
 _putchar(' ');
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
 }
 }
 _putchar('\n');
