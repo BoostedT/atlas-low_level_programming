4093b599d04fd1e21593bd462b308f9fd789c54d
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-24 16:26:46 +0000

    fixed code

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,13 @@
-#include "main.h"
+i#include "main.h"
 /**
-*print_numbers - function that print all numbers from
-*0 to 9
-*
-*Return: returns nothing
-*/
+ * print_numbers - prints numbers from 0 to 9
+ * Return: void
+ */
 void print_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-_putchar(i);
+_putchar(i + '0');
 }
 }
