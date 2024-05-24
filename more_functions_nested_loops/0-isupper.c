bf8ececed13903fff59d5a64cd92c6af95529ab0
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-24 14:07:34 +0000

    added code for 1 and main.h

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * main - entry
- * Return: 0
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase 0 otherwise
  */
-
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
