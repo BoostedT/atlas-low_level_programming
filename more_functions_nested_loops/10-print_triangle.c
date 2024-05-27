545e9b8c466be2029aad583bb9e7c6c08be33159
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-27 19:10:55 +0000

    printing a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@ void print_triangle(int size)
 int i, j, k;
 if (size <= 0)
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
@@ -18,13 +18,13 @@ for (i = 0; i < size; i++)
 {
 for (j = size - i; j > 1; j--)
 {
-putchar(' ');
+_putchar(' ');
 }
 for (k = 0; k <= i; k++)
 {
-putchar('#');
+_putchar('#');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
 }    
