6ff351859d031c1347686dadf2d72a9ba472f96e
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-27 19:29:25 +0000

    added 2 and program to gives length of string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string
- *    * Return: length of string
- *     */
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
 int _strlen(char *s)
 {
 int i = 0;
