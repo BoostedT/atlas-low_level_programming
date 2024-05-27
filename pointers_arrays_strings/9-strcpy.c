47c8512ddd01335ba1f5243b260da436a3e62c56
BoostedT <tyler.whitchurch@atlasschool.com>
2024-05-27 20:14:53 +0000

    copies a string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * *_strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
- *   * @dest: pointer to the buffer in which to copy the string
- *    * @src: pointer to the string to copy
- *     * Return: the pointer to dest
- *      */
+* _strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
+* @dest: pointer to the buffer in which to copy the string
+* @src: pointer to the string to copy
+* Return: the pointer to dest
+*/
 char *_strcpy(char *dest, const char *src)
 {
 int i = 0;
