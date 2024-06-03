b9fb630f3fd3013fbe680efec6f9dc6b0ca6b60c
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-03 13:37:57 -0500

    fixed program on 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,46 +1,42 @@
 #include "main.h"
 /**
  * _strstr - locates a substring
- * @haystack: string to be scanned
- * @needle: string containing the characters to match
- * Return: pointer to the beginning of the located substring, or NULL if the substring is not found
- */
-#include "main.h"
-
-/**
- * _strstr - locates a substring
- * @haystack: string to be scanned
- * @needle: string containing the characters to match
- * Return: pointer to the beginning of the located substring, or NULL if the substring is not found
+ * @haystack: string to search
+ * @needle: substring to search for
+ *
+ * Return: pointer to beginning of located substring or NULL if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-int count = 0, a = 0, b = 0;
+int count = 0, a = 0, k, j = 0, l, i = 0;
+char *p;
 while (needle[count] != '\0')
 {
 count++;
 }
-if (count == 0)
+while (haystack[i] != '\0')
 {
-return haystack; // Return haystack if needle is empty
+i++;
 }
-while (haystack[b] != '\0')
+if (count == 0)
+return (haystack);
+while (haystack[j] != '\0')
 {
-if (haystack[b] == needle[0])
+if (haystack[j] == needle[0])
 {
-for (a = 0; a < count; a++)
+p = &haystack[j];
+l = j;
+a = 0;
+for (k = 0; k < count; k++)
 {
-if (haystack[b + a] != needle[a])
-{
-    break;
+if (haystack[l] == needle[k])
+a++;
+l++;
 }
 }
 if (a == count)
-{
-return (haystack + b);
+return (p);
+j++;
 }
-}
-b++;
-}
-return NULL; // Return NULL if substring is not found
+return ('\0');
 }
