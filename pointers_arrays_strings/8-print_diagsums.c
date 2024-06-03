ef39b052e2254c8cf196301123267e67ea9cdfb3
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-03 13:48:21 -0500

    fixed program

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, j, sum1 = 0, sum2 = 0;
+int i, j, sum1 = 0, sum2 = 0, t, s = 0;
 for (i = 0; i < size; i++)
 {
-sum1 += a[(size + 1) * i];
-sum2 += a[(size - 1) * (i + 1)];
+for (j = 0; j < size; j++)
+{
+if (i == j)
+{
+t = (i * size) + j;
+sum1 += *(a + t);
+}
+if ((i + j) == (size - 1))
+{
+s = (i * size) + j;
+sum2 += *(a + s);
+}
+}
 }
 printf("%d, %d\n", sum1, sum2);
 }
