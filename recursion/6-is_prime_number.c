e43c30aa65802d90b60ad81a8dea3bed61e0c185
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-07 12:16:27 -0500

    redid code to 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,19 +8,12 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return (is_prime(n, 2));
-}
-/**
- * is_prime - This is my function
- * @n: This is my variable
- * @i: This is my variable
- * Return: 1 if the input integer is a prime number, otherwise return 0
- */
-int is_prime(int n, int i)
-{
-if (n == i)
+if (n == 2)
 return (1);
-if (n % i == 0)
+if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
 return (0);
-return (is_prime(n, i + 1));
+else if (n % 9 == 0 || n % 7 == 0)
+return (0);
+else
+return (1);
 }
