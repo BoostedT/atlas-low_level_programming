6393da0379855e6d5b84f68bca4157b40a40b7e1
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-21 10:59:03 -0500

    added

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,23 +13,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-int i, name_len, owner_len;
+int name_len = 0, owner_len = 0, i;
 
+if (name != NULL && owner != NULL)
+{
+name_len = _strlen(name) + 1;
+owner_len = _strlen(owner) + 1;
 new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+
+if (new_dog != NULL)
+{
 return (NULL);
+}
+new_dog->name = malloc(sizeof(char) * name_len);
 
-for (name_len = 0; name[name_len]; name_len++)
-;
-name_len++;
-
-for (owner_len = 0; owner[owner_len]; owner_len++)
-;
-owner_len++;
-
-new_dog->name = malloc(name_len * sizeof(char));
 if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
+}
+new_dog->owner = malloc(sizeof(char) * owner_len);
+
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+new_dog->name = _strcpy(new_dog->name, name);
+new_dog->age = _strcpy(new_dog->owner, owner);
+new_dog->age = age;
+return (new_dog);
 }
