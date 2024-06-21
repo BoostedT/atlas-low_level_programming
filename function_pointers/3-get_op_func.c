86964a86d558038abb98f7fe37140a9e7a408a17
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-21 14:56:40 -0500

    betty style

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,12 +11,11 @@ int op_mod(int a, int b);
  * get_op_func - selects the correct function to perform the operation
  * @s: operator passed as argument to the program
  *
- * Return: pointer to the function that corresponds to the operator given as a parameter
+ * Return: pointer to the function that corresponds to the operator
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = 
-{
+op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
 {"*", op_mul},
