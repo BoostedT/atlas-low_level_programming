c2325eaad44135e680549a2ac0f2e46e3b3218d2
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-21 13:22:33 -0500

    fixed the memory leak in the function new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,39 +2,76 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s++)
+len++;
+return (len);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int index = 0;
+
+for (index = 0; src[index]; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
 /**
  * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (name == NULL || owner == NULL)
-return NULL;
+dog_t *crazy_dog;
 
-    
-int name_len = _strlen(name) + 1;
-int owner_len = _strlen(owner) + 1;
-dog_t *t_dog = malloc(sizeof(dog_t));
-if (t_dog == NULL)
-Return NULL;
-t_dog->name = malloc(sizeof(char) * name_len);
-if (t_dog->name == NULL) {
-free(t_dog);  
-return NULL;
-}
-t_dog->owner = malloc(sizeof(char) * owner_len);
-if (t_dog->owner == NULL) {
-free(t_dog->name);  
-free(t_dog);        
-return NULL;
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+crazy_dog = malloc(sizeof(dog_t));
+if (crazy_dog == NULL)
+return (NULL);
+
+crazy_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (crazy_dog->name == NULL)
+{
+free(crazy_dog);
+return (NULL);
 }
 
-_strcpy(t_dog->name, name);
-_strcpy(t_dog->owner, owner);
-t_dog->age = age;
-return t_dog; 
+crazy_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (crazy_dog->owner == NULL)
+{
+free(crazy_dog->name);
+free(crazy_dog);
+return (NULL);
+}
+
+crazy_dog->name = _strcpy(crazy_dog->name, name);
+crazy_dog->age = age;
+crazy_dog->owner = _strcpy(crazy_dog->owner, owner);
+return (crazy_dog);
 }
