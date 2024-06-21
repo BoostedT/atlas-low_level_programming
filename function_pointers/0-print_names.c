7902e929476107736ccd4ebeca3680cfef3af19d
BoostedT <tyler.whitchurch@atlasschool.com>
2024-06-21 13:52:15 -0500

    added function_pointers project

---
+++ function_pointers/0-print_names.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (f != NULL)
+        f(name);
+}
