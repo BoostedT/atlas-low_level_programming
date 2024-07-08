557859736c92abde7de8f9856af7aaae68990baf
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-08 18:04:45 -0500

    redid the print_binary function to use a loop instead of recursion

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,30 +2,12 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: number to print in binary
- *
+ * @n: number to print
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int mask = 1;
-int flag = 0;
-
-mask <<= 63;
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-while (mask > 0)
-{
-if ((n & mask) == 0 && flag == 1)
-_putchar('0');
-else if ((n & mask) != 0)
-{
-_putchar('1');
-flag = 1;
-}
-mask >>= 1;
-}
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
