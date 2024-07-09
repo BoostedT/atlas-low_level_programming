8597eddff43b02bb41cd0e9ddbe7775b287f136c
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-09 01:12:07 +0000

    fixed 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,23 +10,20 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t n, len = 0;
+int nletters;
+int rwr;
 if (!filename)
-return (-1
-);
-fd = open(filename, O_RDWR | O_APPEND);
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content)
 {
-while (text_content[len])
-len++;
-n = write(fd, text_content, len);
-if (n == -1)
-{
-close(fd);
+for (nletters = 0; text_content[nletters]; nletters++)
+	;
+rwr = write(fd, text_content, nletters);
+if (rwr == -1)
 return (-1);
-}
 }
 close(fd);
 return (1);
