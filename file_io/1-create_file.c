1d9a0ef998d98d7525b28544dc40009f3f438e57
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-08 19:03:52 -0500

    q

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,23 +9,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int fd, len = 0;
+long int wrote;
 if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
+while (text_content[len])
 len++;
-}
-o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (o == -1)
-return (-1);
-w = write(o, text_content, len);
-if (w == -1)
-{
-close(o);
+wrote = write(fd, text_content, len);
+if (wrote == -1)
 return (-1);
 }
-close(o);
+if (close(fd) == -1)
 return (-1);
+return (1);
 }
