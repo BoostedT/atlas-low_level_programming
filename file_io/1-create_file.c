e2268164e88e884d26921c83622abb062a49adfc
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-09 01:09:38 +0000

    fixed #1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,11 +12,9 @@ int create_file(const char *filename, char *text_content)
 int fd;
 int nletters;
 int rwr;
-
 if (!filename)
 return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 if (!text_content)
