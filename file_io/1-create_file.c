c71a07a43f3773730e774cd8a15d48e23fafc990
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-09 00:57:13 +0000

    task 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int rwr;
 if (!filename)
 return (-1);
 
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 if (fd == -1)
 return (-1);
 if (!text_content)
