b5984e8c0a88213ad0c49f9fe01552371c7d4c40
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-08 19:44:27 -0500

    k

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
 /**
- * create_file - creates a file and writes text to it
- * @filename: the name of the file to create
- * @text_content: the text to write to the file
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
 int fd;
 int nletters;
-int lenw;
+int rwr;
 
-if (filename == NULL)
+if (!filename)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-if (text_content == NULL)
-{
-close(fd);
-return (1);
-}
-nletters = 0;
-while (text_content[nletters])
-nletters++;
-lenw = write(fd, text_content, nletters);
-close(fd);
-if (lenw == -1)
+if (!text_content)
+text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++);
+rwr = write(fd, text_content, nletters);
+if (rwr == -1)
 return (-1);
+close(fd);
 return (1);
 }
