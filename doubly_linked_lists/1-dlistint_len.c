1d05acd74bda9a6cfe3d2a379529535cfa2ff3fd
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-15 12:02:34 -0500

    recoded in C 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * @h: pointer to the head of the list
  * Return: number of elements in the list
  */
-size_t dlistint_t_len(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 size_t count = 0;
 while (h)
