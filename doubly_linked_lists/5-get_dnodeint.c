6f5650a71e88458a86f210bc364b1ff31cb3ca4a
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-15 12:15:07 -0500

    kernel style

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node to return
- * Return: the nth node of a dlistint_t linked list, or NULL if it does not exist
+ * Return: the nth node of a dlistint_t linked list, or NULL if it doesnt exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
