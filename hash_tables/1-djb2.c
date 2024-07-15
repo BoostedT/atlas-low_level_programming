975faa0f4423b9df571d4b478f72b8b18024ba19
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-15 18:13:51 -0500

    added djb2 hash function and updated hash_tables.h

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5831
+int c;
+while ((c = *str++))
+hash = ((hash << 5) + hash) + c;
+return (hash);
+}
