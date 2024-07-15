e25f723ac16507389cbb60139c0c747336095765
BoostedT <tyler.whitchurch@atlasschool.com>
2024-07-15 18:15:07 -0500

    possibly fixed compiling error

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -7,7 +7,7 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash = 5831
+unsigned long int hash = 5831;
 int c;
 while ((c = *str++))
 hash = ((hash << 5) + hash) + c;
