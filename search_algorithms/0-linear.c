f504ba03888cb7f1f126c532a94b11ff4fbaa028
Tyler W <tyler.whitchurch@atlasschool.com>
2024-08-09 12:07:30 -0500

    added linear search algorithm and header file for search algorithms

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for an element in an array using linear search
+ * @arr: array to search
+ * @n: number of elements in the array
+ * @x: element to search for
+ * Return: index of the element if found, -1 otherwise
+ */
+int linear_search(int arr[], int n, int x) {
+for (int i = 0; i < n; i++) {
+if (arr[i] == x) {
+return i;
+}
+}
+return -1;
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Function prototypes
+int linear_search(int arr[], int n, int x);
+
+#endif
